5976f2093864b74d1ee3f2044480dcf53571f2bc
bartekma89 <bartekma89@gmail.com>
2018-05-14 14:48:37 +0200

    main section

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,13 @@
 		$(this).toggleClass('active');
 		$('.overlay').toggleClass('open');
 	});
+
+	$('#Vector_Smart_Object').on('click', function(event) {
+		event.preventDefault();
+		const $this = $(this);
+		$this.addClass('doRotate');
+		setTimeout(function() {
+			$this.removeClass('doRotate');
+		}, 1000);
+	});
 })($);
